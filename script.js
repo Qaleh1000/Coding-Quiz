0f920d02d2fe3b583ff4139b68cdc9494b77357c
QS <qaleh.saleh@gmail.com>
2023-11-09 13:18:30 +0000

    added questions to my coding quiz

---
+++ script.js
@@ -0,0 +1,35 @@
+// Creating the questions 
+
+var questions = [
+    {
+        question: "Which of the following is a Javascript data type?",
+        choices: ["Function", "String", "Operator"],
+        correctAnswer: "String"
+    },
+    {
+        question: "What does the strictly equal operator demonstrate?",
+        choices: ["Performs a test to see if two values are equal and of the same data type.",
+        "Add two numbers together or combine two strings.", 
+        "This returns the logically opposite value of what it precedes."],
+        correctAnswer: "Performs a test to see if two values are equal and of the same data type"
+    },
+    {
+        question: "Which is an example of a comment in Javascript?",
+        choices: ["...//...", "//", "!!!!!**"],
+        correctAnswer: "//"
+    },
+    {
+        question: "What is a function?",
+        choices: ["Containers for named values", 
+        "A block of reusable code written to perform a specific task.", 
+        "Are actions that can be performed on objects."],
+        correctAnswer: "A block of reusable code written to perform a specific task."
+    },
+
+    {
+        question: "Which of the following allows you to convert a string to a native object?",
+        choices: ["JSON.convert", "JSON.parse", "JSON.stringify"],
+        correctAnswer: "JSON.parse"
+    },
+];
+// 
